841ae7e802d468334b46ebf97092135071c844cf
shengzx <937176548@qq.com>
2016-09-21 17:59:25 +0800

    注册页面的样式写好了
    感觉 div flout 就是个坑。而且一入坑门深似海

--- angular/directive.js
+++ angular/directive.js
@@ -1,56 +1,27 @@
-app.controller("mianmenuCtrl", ['$scope', 'website', function ($scope, website) {
-    var params = {
-        Type: "Get",
-        Url: "/Angular/Json/bootmenu.json"
-    };
-    var params1 = {
-        Type: "Get",
-        Url: "/Angular/Json/route.json"
-    };
-    var dos = [{
-        params: params,
-        Name: "Dataload"
-    }, {
-            params: params1,
-            Name: "GetRoute"
-        }];
-    var menucompent = new compent(website, dos, "menucompent");
-    menucompent.Dataload(function (data) {
-        $scope.model = data;
-    });
-    menucompent.GetRoute();
-}]);
-app.directive("mianmenu", ['website', function (website) {
+'use strict'
+var app = angular.module('app');
+app.directive("tNav", [function() {
     return {
-        restrict: "E",
-        template: function (e, a) {
-            return '<div class="' + a.menuClass + ' sidebar-shortcuts" id="sidebar-shortcuts"><div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large"><h5>' + a.menuLable + '</h5></div><ul class="nav ' + a.menuClass + ' nav-list"><li class="hsub" ng-repeat="menu in model.Menu"><a ng-href="{{menu.Url?menu.Url:\'javascript:void(0)\'}}" class="dropdown-toggle"><i class="{{menu.icon}}"></i><span class="menu-text"> {{menu.MenuName}}</span><b class="arrow glyphicon glyphicon-menu-down"></b></a><b class="arrow"></b>'
-                + '<ul class="submenu" ng-if="menu.Child">'
-                + '<li class="" ng-repeat="list in menu.Child"><a href="{{list.Url}}"><i class="menu-icon fa fa-caret-right"></i>{{list.MenuName}}</a><b class="arrow"></b></li></ul>'
-                + '</li></ul>';
+        restrict: "A",
+        template: function(e, a) {
+            return '<div class="y-l-t">' +
+                '<div class="logo">' +
+                '<span class="y-logo y-logo-sm">痒</span>' +
+                '</div>' +
+                '<div class="y-l-w">' +
+                '<span>欢迎痒痒的小伙伴～</span>' +
+                '</div>' +
+                '<div class="y-l-t-uidiv">' +
+                '<div>' +
+                '<ul>' +
+                '<li><a href="#" class="btn">痒痒漫画</a></li>' +
+                '<li><a ui-sref="register" class="btn">成为会员</a></li>' +
+                '<li><a href="#" class="btn">会员登录</a></li>' +
+                '</ul>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
         },
-        replace: true,
-        controller: "mianmenuCtrl",
-        link: function ($scope, e, a) {
-            e.on("click", "a", function () {
-                if ($(this).parent().hasClass("hsub")) {
-                    if ($(this).parent().hasClass("open")) {
-                        $(this).parent().removeClass("open");
-                        $(this).parent().children('ul').slideUp();
-                    } else {
-                        $("#sidebar li").children('ul').slideUp();
-                        $("#sidebar li").removeClass("open");
-                        $(this).parent().addClass("open");
-                        var s = $(this).parent().children('ul').slideDown();
-                    }
-                } else {
-                    $("#sidebar li").removeClass("active");
-                    $(this).parent().addClass("active");
-                    if ($(this).parent().parent().parent().hasClass("hsub")) {
-                        $(this).parent().parent().parent().addClass("active");
-                    }
-                }
-            });
-        }
-    };
+        replace: true
+    }
 }]);
--- view/home/script/Home_Controller.js
+++ view/home/script/Home_Controller.js
@@ -4,13 +4,13 @@
 //     $scope.label="fuck";
 // }
 var app = angular.module('app');
-app.controller('homeCtrl', ['$scope', 'pageService', function ($scope, pageService) {
-    $scope.event = pageService;
-    $scope.event.msg = pageService.msg("homepage alert", "msg_alert");
-    $scope.event.getcookie = function () {
-        $scope.event.cookie("userinfo", "get");
-    }
-    $scope.event.instanceFile({name:'rname',url:'rurl',data:'rdata'},function(data){
-    });
-    $scope.label = "fuck";
+app.controller('homeCtrl', ['$scope', 'pageService', function($scope, pageService) {
+    // $scope.event = pageService;
+    // $scope.event.msg = pageService.msg("homepage alert", "msg_alert");
+    // $scope.event.getcookie = function () {
+    //     $scope.event.cookie("userinfo", "get");
+    // }
+    // $scope.event.instanceFile({name:'rname',url:'rurl',data:'rdata'},function(data){
+    // });
+    // $scope.label = "fuck";
 }]);
